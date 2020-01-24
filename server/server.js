1c180817fc3b2d68157043cc3bbc14c11acd876e
brendanbond <brendanbond@gmail.com>
2020-01-24 10:46:39 -0800

    added bare bones socket.io implementation

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const http = require("http");
 const morgan = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+/* TODO: we will likely need a game object for connections to the server while a game is in progress */
+const game = {};
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
@@ -11,6 +15,12 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
-app.listen(port, () => {
-  console.log("Server up and listening on port " + port);
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", socket => {
+  console.log("New client connected.");
+  socket.on("disconnect", () => {
+    console.log("Client disconnected.");
+  });
 });
