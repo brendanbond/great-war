9a493a4fbe13a050b728e8de818e3817d2d86b36
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-03 19:53:58 -0600

    Created the Alfil piece and added the movement logic

---
+++ game/Alfil.js
@@ -0,0 +1,43 @@
+﻿/*
+* ****************************************************************************************
+*                                            Alfil
+* ****************************************************************************************
+* 
+* This piece will be moving in a manner similar to bishops, in that they move diagonally,
+* however, they have to move an exact number instead of any number available
+* 
+* Movement and Capturing for this piece is:
+*       exactly two (2) spaces in any diagonal
+* */
+
+const utils = require('./utils');
+
+function Alfil(color) {
+  this.id = 'A';
+  this.color = color;
+}
+
+Alfil.prototype.getActions = function (board, row, col) {
+  let actions = {
+    moves: []
+  };
+
+  //check the diagonals
+  for (let r = row - 2; r <= row + 2; r += 4) {
+    for (let c = col - 2; c <= col + 2; c += 4) {
+      if (board.isValidPosition(r, c)) {
+        if (board.isOccupiedPosition(r, c)) {
+          let target = board.positionAt(r, c);
+          
+          if (utils.areOppositeColors(this, target)) {
+            actions.moves.push([r, c]);
+          }
+        } else {
+          actions.moves.push([r, c]);
+        }//end vacancy check
+      }//end valid space check
+    }//end column manipulation loop
+  }//end row manipulation loop
+};
+
+module.exports = Alfil;
