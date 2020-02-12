8f0f4febdf82fc100b2d9966315cc1ef939c4181
brendanbond <brendanbond@gmail.com>
2020-02-12 09:11:59 +0100

    remove typo

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
-import React, { createContext } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-import SocketIOClient from "socket.io-client";
 
 ReactDOM.render(<App />, document.getElementById("root"));
