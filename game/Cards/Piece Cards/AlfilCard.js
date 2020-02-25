8ab11de057470ec025460c537c76e0e6ba9a202f
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-24 20:21:54 -0600

    Created Alfil Card

---
+++ game/Cards/Piece Cards/AlfilCard.js
@@ -0,0 +1,11 @@
+﻿function AlfilCard() {
+  
+}
+
+AlfilCard.prototype.placePiece = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if(!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Alfil(color));
+    }
+  }
+};
