de589339c456bbaa76e5a6a4667d053cce53c807
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-24 20:22:59 -0600

    Created Prince Card

---
+++ game/Cards/Piece Cards/PrinceCard.js
@@ -0,0 +1,11 @@
+﻿function PrinceCard() {
+  
+}
+
+PrinceCard.prototype.placePrince = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Prince(color));
+    }
+  }
+};
