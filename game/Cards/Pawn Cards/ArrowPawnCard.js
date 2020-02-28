c8eaf98f8cac34e2bc365abf21600be845003a7b
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:49:33 -0600

    Needed to add a parameter to the prototype

--- game/Cards/Pawn Cards/ArrowPawnCard.js
+++ game/Cards/Pawn Cards/ArrowPawnCard.js
@@ -1,5 +1,5 @@
 ﻿function ArrowPawnCard() {}
 
-ArrowPawnCard.prototype.place =  (row, col) => {
+ArrowPawnCard.prototype.place =  (board, row, col) => {
   /* TODO: Do pawn cards act as piece cards (placing) or effect cards (selecting)? */
 };
