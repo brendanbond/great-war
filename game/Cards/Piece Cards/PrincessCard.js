6690c55f1635c427940d48f5736ad463cf1a73a5
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:36:43 -0600

    Created Princess Piece card

---
+++ game/Cards/Piece Cards/PrincessCard.js
@@ -0,0 +1,11 @@
+﻿function PrincessCard() {
+
+}
+
+PrincessCard.prototype.placePrincess = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Princess(color));
+    }
+  }
+};
