a4f03fb7d3cf9fac0d4d50303ce6bc52a46b9b77
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:35:34 -0600

    Created Gargoyle Piece card

---
+++ game/Cards/Piece Cards/GargoyleCard.js
@@ -0,0 +1,11 @@
+﻿function GargoyleCard() {
+
+}
+
+GargoyleCard.prototype.placeGargoyle = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Gargoyle(color));
+    }
+  }
+};
