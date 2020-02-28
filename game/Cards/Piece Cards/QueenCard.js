5a0bf5691b4fcc708d9c1a9baf65f0920319cb20
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:37:08 -0600

    Created Queen Piece card

---
+++ game/Cards/Piece Cards/QueenCard.js
@@ -0,0 +1,11 @@
+﻿function QueenCard() {
+
+}
+
+QueenCard.prototype.placeQueen = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Queen(color));
+    }
+  }
+};
