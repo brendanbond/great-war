4b18176f5e12d2f9c2489cbe479eaec24d42ee72
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:33:19 -0600

    Created Dabbaba Piece card

---
+++ game/Cards/Piece Cards/DabbabaCard.js
@@ -0,0 +1,11 @@
+﻿function DabbabaCard() {
+
+}
+
+DabbabaCard.prototype.placeDabbaba = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Dabbaba(color));
+    }
+  }
+};
