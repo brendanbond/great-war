3043b99c7e32884984fd40706a5ed387f4022a79
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:34:46 -0600

    Created False Trap Effect card

---
+++ game/Cards/Effect Cards/FalseTrapCard.js
@@ -0,0 +1,7 @@
+﻿function FalseTrapCard() {}
+
+FalseTrapCard.prototype.set = (row, col) => {
+  if (board.isValidPosition(row, col) && !board.isOccupiedPosition(row, col)) {
+    board.setPosition(row, col, new Trap(row, col, false));
+  }
+};
