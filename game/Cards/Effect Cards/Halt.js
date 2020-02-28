19a692b0aa643ded0c36d2d114ad5e2804f05fc4
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:36:03 -0600

    Created Halt Effect card

---
+++ game/Cards/Effect Cards/Halt.js
@@ -0,0 +1,17 @@
+﻿const utils = require('../../utils.js');
+
+function HaltCard() {
+
+}
+
+HaltCard.prototype.haltPiece = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (board.isOccupiedPosition(row, col)) {
+      let target = board.positionAt(row, col);
+      
+      if (utils.areOppositeColors(color, target)) {
+        /* TODO: How to make the piece not able to move? */
+      }
+    }
+  }
+};
