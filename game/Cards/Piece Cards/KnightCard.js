08704c54ec9da22ea23efe346f1e99027b95ab3d
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 08:59:16 -0600

    Created Knight Card

---
+++ game/Cards/Piece Cards/KnightCard.js
@@ -0,0 +1,11 @@
+﻿function KnightCard() {
+
+}
+
+KnightCard.prototype.placeKnight = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Knight(color));
+    }
+  }
+};
