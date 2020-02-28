39c357c6935368219f25424532bac56a3fc03712
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:41:06 -0600

    Created Simple Pawn Piece card

---
+++ game/Cards/Piece Cards/SimplePawnCard.js
@@ -0,0 +1,11 @@
+﻿function SimplePawnCard() {
+
+}
+
+SimplePawnCard.prototype.placeSimplePawn = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Pawn(color));
+    }
+  }
+};
