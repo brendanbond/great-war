24f3b7685c7fe630efaad7d5c86e332f7d019549
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:32:57 -0600

    Created Citizen Piece card

---
+++ game/Cards/Piece Cards/CitizenCard.js
@@ -0,0 +1,10 @@
+﻿function CitizenCard() {
+
+}
+CitizenCard.prototype.placeCitizen = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Citizen(color));
+    }
+  }
+};
