752e39858ae92c4ceb265414727dbd1659ca0b71
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:51:04 -0600

    Needed to add a parameter to the prototype

--- game/Cards/Effect Cards/FalseTrampolineCard.js
+++ game/Cards/Effect Cards/FalseTrampolineCard.js
@@ -2,7 +2,7 @@
 
 }
 
-FalseTrampolineCard.prototype.place = (row, col) => {
+FalseTrampolineCard.prototype.place = (board, row, col) => {
   if (board.isValidPosition(row, col) && !board.isOccupiedPosition(row, col)) {
     board.setPosition(row, col, new Tramopline(row, col, false));
   }
