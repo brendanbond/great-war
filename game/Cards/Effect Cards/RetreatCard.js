09d632fbddb48ce74de03579c0a4a592bdc0215b
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:37:33 -0600

    Created Retreat Effect card

---
+++ game/Cards/Effect Cards/RetreatCard.js
@@ -0,0 +1,9 @@
+﻿const utils = require('../../utils');
+
+function RetreatCard() {
+  
+}
+
+RetreatCard.prototype.retreat = (color) => {
+  /* TODO: How to implement moving all friendly pieces to king? */
+};
