d766d6f0237eb6660ae99d2a0e40863696baf428
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:38:43 -0600

    Created Rockslide Effect card

---
+++ game/Cards/Effect Cards/RockslideCard.js
@@ -0,0 +1,23 @@
+﻿const utils = require('../../utils');
+
+function RockslideCard() {
+  
+}
+
+RockslideCard.prototype.rockslide = (row, col, player) => {
+  if (board.isValidPosition(row, col)) {
+    if (board.isOccupiedPosition(row, col)) {
+      let target = board.positionAt(row, col);
+      
+      if (utils.areOppositeColors(player, target)) {
+        if (utils.isMassive(target)) {
+          /* TODO: Somehow check if piece is massive piece */
+          board.clearPosition(row, col);
+          /* TODO: implement capture methods */
+        } else {
+          /* TODO: Implement halt like effect to piece */
+        }
+      }
+    }
+  }
+};
