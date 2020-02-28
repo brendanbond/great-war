bc47b817d84d8585bc4cdf07b6fb75fc855fe312
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:45:31 -0600

    Created Trampoline Effect card and a Trampoline Object to be shared between the Trampoline and False Trampoline Cards

---
+++ game/Cards/Effect Cards/Trampoline.js
@@ -0,0 +1,34 @@
+﻿/*
+ * This is a shared class for trampoline and false trampoline cards
+ * If there is a better and more efficient way to implement these two cards,
+ * then this class is not needed at all.
+ * 
+ * It has a clear function internally to remove the trampoline after use automatically,
+ * and will handle to movement checks for if the movement causes Check
+ */
+
+const utils = require('../../utils');
+const location = [];
+
+function Trampoline(row, col, real) {
+  location[0] = row;
+  location[1] = col;
+  this.realTramp = real;
+}
+
+function jump(piece, target) {
+  if (this.realTramp) {
+    //real trampoline
+    if (utils.areOppositeColors(piece, target)) {
+      /* TODO: Determine how to check for king and whether the trampoline will cause check */
+    }
+    this.jumped();
+  } else {
+    //false trampoline
+    /* TODO: Determine what the false trampoline can hide on the board */
+  }
+}
+
+function jumped() {
+  board.clearPosition(location[0], location[1]);
+}
---
+++ game/Cards/Effect Cards/TrampolineCard.js
@@ -0,0 +1,9 @@
+﻿function TrampolineCard() {
+  
+}
+
+TrampolineCard.prototype.place = (board, row, col) => {
+  if (board.isValidPosition(row, col) && !board.isOccupiedPosition(row, col)) {
+    board.setPosition(row, col, new Tramopline(row, col, true));
+  }
+};
