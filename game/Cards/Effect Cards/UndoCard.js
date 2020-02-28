4889bdba81e8f9cf346fb2430056607272e9fe51
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:46:47 -0600

    Created Undo Effect card

---
+++ game/Cards/Effect Cards/UndoCard.js
@@ -0,0 +1,9 @@
+﻿function UndoCard() {}
+
+UndoCard.prototype.undo = (board, row, col) => {
+  if (board.isValidPosition(row, col) && board.isOccupiedPosition(row, col)) {
+    /* TODO: Implement the undo of the movement and captures 1 turn prior */
+    /* TODO: Implement a halt like effect on the selected piece to keep from moving for a turn */
+    /* TODO: Is the pawn sacrifice handled here or elsewhere? */
+  }
+};
