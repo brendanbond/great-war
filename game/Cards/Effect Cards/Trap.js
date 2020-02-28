7abdfcc05cc9cbe5b4bbe2239ba2e0525a477b03
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:46:13 -0600

    Created Trap Effect card and a Trap Object to be shared between the Trap and False Trap Cards

---
+++ game/Cards/Effect Cards/Trap.js
@@ -0,0 +1,25 @@
+﻿/*
+ * A class for both the trap and false trap cards.
+ * If there is a better and more efficient way to implement both of these cards,
+ * this class will not be needed
+ * 
+ * This class has a function the spring the trap when a piece lands on it
+ */
+const location = [];
+
+function Trap(row, col, realTrap) {
+  location[0] = row;
+  location[1] = col;
+  this.realTrap = realTrap;
+}
+
+function spring() {
+  if (this.realTrap) {
+    //real trap
+    /* TODO: Determine how to handle the captures when the trap is sprung */
+    board.clearPosition(location[0], location[1]);
+  } else {
+    //false trap
+    /* TODO: Determine what can be hidden on the board by this card */
+  }
+}
---
+++ game/Cards/Effect Cards/TrapCard.js
@@ -0,0 +1,9 @@
+﻿function TrapCard() {
+  
+}
+
+TrapCard.prototype.set = (board, row, col) => {
+  if (board.isValidPosition(row, col) && !board.isOccupiedPosition(row, col)) {
+    board.setPosition(row, col, new Trap(row, col, true));
+  }
+};
