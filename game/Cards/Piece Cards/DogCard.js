cb9dc06f76029cffe41b2065450054742a52f746
Andrew Randle-Warren <arandlemiller97@yahoo.com>
2020-02-28 12:33:38 -0600

    Created Dog Piece card

---
+++ game/Cards/Piece Cards/DogCard.js
@@ -0,0 +1,11 @@
+﻿function DogCard() {
+
+}
+
+DogCard.prototype.placeDog = (board, row, col, color) => {
+  if (board.isValidPosition(row, col)) {
+    if (!board.isOccupiedPosition(row, col)) {
+      board.setPosition(row, col, new Dog(color));
+    }
+  }
+};
