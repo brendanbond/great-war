a7692f34de05f9eab3ef82f7656f595c3853d93e
ajbond <olishmollie@gmail.com>
2020-03-15 17:00:48 -0500

    Refactor pawn moves/attacks

--- game/Pawn.js
+++ game/Pawn.js
@@ -4,7 +4,6 @@ function Pawn(color) {
   Piece.call(this, "P", color);
   this.symbol = this.color == "white" ? "\u2659" : "\u265f";
   this.opening = true;
-  this.attacks = []; // Attacks differ from moves.
 }
 
 Pawn.prototype = Object.create(Piece.prototype);
@@ -16,28 +15,27 @@ Object.defineProperty(Pawn.prototype, "constructor", {
 });
 
 Pawn.prototype.updateMoves = function(board, row, col) {
+  // Reset moves so that we're not just pushing more moves onto the array.
   this.moves = [];
 
-  if (this.color === "white") {
-    this.moves.push([row - 1, col]);
-    if (this.opening) {
-      this.moves.push([row - 2, col]);
+  // White moves up, black moves down.
+  let k = this.isWhite() ? -1 : 1;
+
+  // Add moves.
+  if (board.isEmptyPosition(row + k, col)) {
+    this.moves.push([row + k, col]);
+    if (this.opening && board.isEmptyPosition(row + 2 * k, col)) {
+      this.moves.push([row + 2 * k, col]);
     }
-    this.attacks.push([row - 1, col + 1], [row - 1, col - 1]);
-  } else {
-    this.moves.push([row + 1, col]);
-    if (this.opening) {
-      this.moves.push([row + 2, col]);
-    }
-    this.attacks.push([row + 1, col + 1], [row + 1, col - 1]);
   }
 
-  this.moves = this.moves.filter(move => {
-    return (
-      board.isValidPosition(move[0], move[1]) &&
-      board.isEmptyPosition(move[0], move[1])
-    );
-  });
+  // Add attacks.
+  if (board.isOccupiedPosition(row + k, col + 1)) {
+    this.moves.push([row + k, col + 1]);
+  }
+  if (board.isOccupiedPosition(row + k, col - 1)) {
+    this.moves.push([row + k, col - 1]);
+  }
 };
 
 Pawn.prototype.move = function() {
